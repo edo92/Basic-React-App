676446fc2fed18ef52435fd91010a09c26cfeb4c
Eduard Jacobs <edoter92@gmail.com>
2021-12-10 09:26:11 -0800

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Testing this Application V1</h1>
+        <h1>Testing this Application V2</h1>
       </header>
     </div>
   );
